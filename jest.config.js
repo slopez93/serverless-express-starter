d2ed9113de7264b58cabb604a7e640721859b02c
Santi López Bayo <slopez@MacBook-Pro-de-Santi-2.local>
2022-01-03 20:12:11 +0800

    fix: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node'
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '.*\\.test.(ts|tsx|js)',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
